f3b5c4c0ca01b666e95fb4471abb488095607a33
JacobM1995 <99050215+JacobM1995@users.noreply.github.com>
2023-06-23 15:06:35 -0700

    Add files via upload

---
+++ Install Adobe Pro 32 bit.ps1
@@ -0,0 +1,20 @@
+﻿Set-ExecutionPolicy Undrestricted -Force
+
+New-Item -Path "C:\" -Name "PowerShell" -Item "directory"
+New-Item -Path "C:\PowerShell" -Name "Zip" -Item "directory"
+New-Item -Path "C:\PowerShell\Zip" -Name "ZipFile" -Item "directory"
+New-Item -Path "C:\PowerShell\Zip" -Name "UnZipFiles" -Item "directory"
+
+
+$Url = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_WWMUI.zip"
+$DownloadZipFile = "C:\PowerShell\Zip\ZipFile\" + $(Split-Path -Path $Url -Leaf)
+$ExtractPath = "C:\PowerShell\Zip\UnZipFiles\"
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+$ExtractShell = New-Object -ComObject Shell.Application 
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process $ExtractPath -wait
+
+Start-Process "C:\Powershell\Zip\UnZipFiles\Adobe Acrobat\setup.exe" -force -wait
+
+Set-ExecutionPolicy Undefined -Force
---
+++ Install VLC Media Player.ps1
@@ -0,0 +1,12 @@
+﻿$testchoco = powershell choco -v
+if(-not($testchoco)){
+    Set-ExecutionPolicy Unrestricted -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    choco install vlc --force -y
+    Set-ExecutionPolicy Undefined -Force
+}
+else{
+    Set-ExecutionPolicy Unrestricted -Force
+    choco install vlc --force -y
+    Set-ExecutionPolicy Undefined -Force
+}
