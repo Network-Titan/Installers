ab3a0547c9564a0634aa579d4be4940e18a29cbf
JacobM1995 <99050215+JacobM1995@users.noreply.github.com>
2023-08-03 10:28:54 -0700

    Update Install Adobe Pro 32 bit.ps1

--- Install Adobe Pro 32 bit.ps1
+++ Install Adobe Pro 32 bit.ps1
@@ -1,20 +1,22 @@
 ﻿Set-ExecutionPolicy Undrestricted -Force
 
-New-Item -Path "C:\" -Name "PowerShell" -Item "directory"
-New-Item -Path "C:\PowerShell" -Name "Zip" -Item "directory"
-New-Item -Path "C:\PowerShell\Zip" -Name "ZipFile" -Item "directory"
-New-Item -Path "C:\PowerShell\Zip" -Name "UnZipFiles" -Item "directory"
+New-Item -Path "C:\" -Name "NetworkTitan" -Item "directory"
+New-Item -Path "C:\NetworkTitan\" -Name "Adobe32" -Item "directory"
+New-Item -Path "C:\NetworkTitan\Adobe32\" -Name "PowerShell" -Item "directory"
+New-Item -Path "C:\NetworkTitan\Adobe32\PowerShell" -Name "Zip" -Item "directory"
+New-Item -Path "C:\NetworkTitan\Adobe32\PowerShell\Zip" -Name "ZipFile" -Item "directory"
+New-Item -Path "C:\NetworkTitan\Adobe32\PowerShell\Zip" -Name "UnZipFiles" -Item "directory"
 
 
 $Url = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_WWMUI.zip"
-$DownloadZipFile = "C:\PowerShell\Zip\ZipFile\" + $(Split-Path -Path $Url -Leaf)
-$ExtractPath = "C:\PowerShell\Zip\UnZipFiles\"
+$DownloadZipFile = "C:\NetworkTitan\Adobe32\PowerShell\Zip\ZipFile\" + $(Split-Path -Path $Url -Leaf)
+$ExtractPath = "C:\NetworkTitan\Adobe32\PowerShell\Zip\UnZipFiles\"
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
 $ExtractShell = New-Object -ComObject Shell.Application 
 $ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
 $ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
 Start-Process $ExtractPath -wait
 
-Start-Process "C:\Powershell\Zip\UnZipFiles\Adobe Acrobat\setup.exe" -ArgumentList "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory "C:\Powershell" -wait
+Start-Process "C:\NetworkTitan\Adobe32\Powershell\Zip\UnZipFiles\Adobe Acrobat\setup.exe" -ArgumentList "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory "C:\NetworkTitan\Adobe32\Powershell" -wait
 
 Set-ExecutionPolicy Undefined -Force
