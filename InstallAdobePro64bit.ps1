ee02c67a7047432311baa7232e780d35aaba5d73
Chris Nolan <72170535+cnolan-nt@users.noreply.github.com>
2023-08-03 16:49:31 -0600

    Remove Execution Policy and cleanup folder names

--- InstallAdobePro64bit.ps1
+++ InstallAdobePro64bit.ps1
@@ -1,20 +1,15 @@
-﻿Set-ExecutionPolicy Undrestricted -Force
+﻿New-Item -Path "C:\NetworkTitan\Adobe64\Powershell\Zip\ZipFiles" -ItemType Directory -Force
+New-Item -Path "C:\NetworkTitan\Adobe64\Powershell\Zip\UnZipFiles" -ItemType Directory -Force
 
-New-Item -Path "C:\" -Name "PowerShell" -Item "directory"
-New-Item -Path "C:\PowerShell" -Name "Zip" -Item "directory"
-New-Item -Path "C:\PowerShell\Zip" -Name "ZipFile" -Item "directory"
-New-Item -Path "C:\PowerShell\Zip" -Name "UnZipFiles" -Item "directory"
 
 
 $Url = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip"
-$DownloadZipFile = "C:\PowerShell\Zip\ZipFile\" + $(Split-Path -Path $Url -Leaf)
-$ExtractPath = "C:\PowerShell\Zip\UnZipFiles\"
+$DownloadZipFile = "C:\NetworkTitan\Adobe64\Powershell\Zip\ZipFiles\" + $(Split-Path -Path $Url -Leaf)
+$ExtractPath = "C:\NetworkTitan\Adobe64\Powershell\Zip\UnZipFiles\"
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
 $ExtractShell = New-Object -ComObject Shell.Application 
 $ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
 $ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
 Start-Process $ExtractPath -wait
 
-Start-Process "C:\Powershell\Zip\UnZipFiles\Adobe Acrobat\setup.exe" -ArgumentList "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory "C:\Powershell" -wait
-
-Set-ExecutionPolicy Undefined -Force
+Start-Process "C:\NetworkTitan\Adobe64\Powershell\Zip\UnZipFiles\Adobe Acrobat\setup.exe" -ArgumentList "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory "C:\NetworkTitan\Adobe64\Powershell" -wait
