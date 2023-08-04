068350f0b6dd4415ed991f8e0ab412d736bfafdd
JacobM1995 <99050215+JacobM1995@users.noreply.github.com>
2023-08-04 16:05:01 -0700

    Update Install7Zip.ps1

--- Install7Zip.ps1
+++ Install7Zip.ps1
@@ -1,14 +1,10 @@
 
 $testchoco = powershell choco -v
 if(-not($testchoco)){
-    Set-ExecutionPolicy Unrestricted -Force
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     choco install 7zip.install --force -y
-    Set-ExecutionPolicy Undefined -Force
 }
 else{
-    Set-ExecutionPolicy Unrestricted -Force
     choco install 7zip.install --force -y
-    Set-ExecutionPolicy Undefined -Force
 }
 
