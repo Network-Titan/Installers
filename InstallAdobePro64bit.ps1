28e5899c123437251929420b38a5204179e89862
Chris Nolan <72170535+cnolan-nt@users.noreply.github.com>
2023-08-04 09:14:53 -0600

    Cleanup paths using variables

--- InstallAdobePro64bit.ps1
+++ InstallAdobePro64bit.ps1
@@ -1,15 +1,18 @@
-﻿New-Item -Path "C:\NetworkTitan\Adobe64\Powershell\Zip\ZipFiles" -ItemType Directory -Force
-New-Item -Path "C:\NetworkTitan\Adobe64\Powershell\Zip\UnZipFiles" -ItemType Directory -Force
+﻿$NTAppPath = "C:\NetworkTitan\Acrobat64"
 
+$ZipPath = "$($NTAppPath)\Zip\ZipFiles"
+$UnZipPath = "$($NTAppPath)\Zip\UnZipFiles"
 
+New-Item -Path $ZipPath -ItemType Directory -Force
+New-Item -Path $UnZipPath -ItemType Directory -Force
 
 $Url = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip"
-$DownloadZipFile = "C:\NetworkTitan\Adobe64\Powershell\Zip\ZipFiles\" + $(Split-Path -Path $Url -Leaf)
-$ExtractPath = "C:\NetworkTitan\Adobe64\Powershell\Zip\UnZipFiles\"
+$DownloadZipFile = $ZipPath + $(Split-Path -Path $Url -Leaf)
+$ExtractPath = $UnZipPath
 Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
 $ExtractShell = New-Object -ComObject Shell.Application 
 $ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
 $ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
 Start-Process $ExtractPath -wait
 
-Start-Process "C:\NetworkTitan\Adobe64\Powershell\Zip\UnZipFiles\Adobe Acrobat\setup.exe" -ArgumentList "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory "C:\NetworkTitan\Adobe64\Powershell" -wait
+Start-Process "$($UnZipPath)\Adobe Acrobat\setup.exe" -ArgumentList "/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES" -WorkingDirectory $NTAppPath -wait
