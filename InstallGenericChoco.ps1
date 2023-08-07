cd2d61acdb2cd4c05c95d52b772a13bd3b90a3a4
Chris Nolan <72170535+cnolan-nt@users.noreply.github.com>
2023-08-07 09:07:57 -0600

    Create Generic Choco installer

---
+++ InstallGenericChoco.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [string]$AppName #Name of the App being installed, so the install script can be downloaded into this folder. Don't use spaces.
+)
+
+# Check if Choco is installed already. If not, install it.
+$testchoco = powershell choco -v
+if(-not($testchoco)){
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+# Use choco to install the selected app.
+choco install $AppName --force -y
