a5a5a7db150bc101f65d52190f73907d6f6e2b12
Chris Nolan <72170535+cnolan-nt@users.noreply.github.com>
2023-08-07 12:07:08 -0600

    Create InstallCWAutomateControlCenter.ps1

---
+++ InstallCWAutomateControlCenter.ps1
@@ -0,0 +1,16 @@
+﻿# Working directory to download installer, unzip it, and run from here
+$NTAppPath = "C:\NetworkTitan\Automate"
+New-Item -Path $NTAppPath -ItemType Directory -Force
+
+# Path to Network Titan Connectwise Automate Control Center installer
+$Url = "https://monitor.networktitan.com/LabTech/Updates/ControlCenterInstaller.exe"
+
+# Download the file to our working directory
+$DownloadInstallFile = $NTAppPath + "\" + $(Split-Path -Path $Url -Leaf)
+Invoke-WebRequest -Uri $Url -OutFile $DownloadInstallFile
+
+# Run silent installation
+Start-Process "$($NTAppPath)\ControlCenterInstaller.exe" -ArgumentList "/s" -wait
+
+# Delete working directory and files to save disk space
+Remove-Item $NTAppPath -Recurse -Force
