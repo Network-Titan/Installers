b71b1b450a55e8d89b237c923a7ced9077a532b7
Chris Nolan <72170535+cnolan-nt@users.noreply.github.com>
2023-08-18 09:59:03 -0600

    Quote package-parameters

--- InstallGenericChoco.ps1
+++ InstallGenericChoco.ps1
@@ -26,7 +26,7 @@ if ($Uninstall){
 }
 Else{
     # If Uninstall is not set, use choco to install the selected app.
-    choco install --debug --verbose $AppName $ExtraParameters --force -y
+    choco install --debug --verbose $AppName "$($ExtraParameters)" --force -y
 }
 
 
