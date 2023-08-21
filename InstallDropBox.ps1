d065294aa2fcd325062cf3f418ee750592cd67e7
JacobM1995 <99050215+JacobM1995@users.noreply.github.com>
2023-08-21 14:37:30 -0700

    Update InstallDropBox.ps1

--- InstallDropBox.ps1
+++ InstallDropBox.ps1
@@ -1,7 +1,7 @@
 $testchoco = powershell choco -v
 if(-not($testchoco)){
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    choco install 7zip.install --force -y
+    choco install dropbox --force -y
 }
 else{
     choco install dropbox --force -y
